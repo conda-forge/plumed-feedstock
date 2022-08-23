1455fba3f9af0b9a9975aef6c5383c6f268bacef
Toni G <tonigi@users.noreply.github.com>
2022-08-23 03:07:18 +0200

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,6 @@
-bash configure --prefix=%PREFIX% --disable-openmp
+cygpath -u %PREFIX% > prefix.txt
+set /p UNIX_PREFIX=<prefix.txt
+
+bash configure --prefix=%UNIX_PREFIX% --disable-openmp
 make
 make install
